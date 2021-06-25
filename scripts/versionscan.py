60b0e0be0e453d54135a029385e6b457c8e453e4
Developer From Jokela <40487414+developerfromjokela@users.noreply.github.com>
2021-06-25 16:24:08 +0300

    Update versionscan.py

--- scripts/versionscan.py
+++ scripts/versionscan.py
@@ -12,7 +12,7 @@ versions = {
 
 knownOldestVersion = 217
 latestUrl = "https://static.abitti.fi/usbimg/prod/latest.txt"
-filePath = "public/versions.json"
+filePath = "docs/versions.json"
 
 
 class DateTimeEncoder(json.JSONEncoder):
