04f1a10aa550958f6b97d13ceb05bdce3e523f83
developerfromjokela <developerfromjokela@gmail.com>
2021-07-01 13:45:15 +0300

    Updated script to include beta boolean, if version was not found in release notes, or it has not been released yet.

--- scripts/versionscan.py
+++ scripts/versionscan.py
@@ -1,4 +1,5 @@
 import json
+import re
 from datetime import datetime
 
 import remotezip
@@ -10,9 +11,17 @@ versions = {
     "koe": []
 }
 
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/5320 (KHTML, like Gecko) Chrome/39.0.843.0 Mobile '
+                  'Safari/5320',
+}
+
 knownOldestVersion = 217
 latestUrl = "https://static.abitti.fi/usbimg/prod/latest.txt"
+releaseNotesUrl = "https://www.abitti.fi/fi/paivitykset/parannukset/digabios-palvelintikku-opiskelijan-tikku/"
 filePath = "docs/versions.json"
+
+releasePattern = re.compile("((ABITTI|SERVER)[0-9]{4}[A-Z0-9])")
 
 
 class DateTimeEncoder(json.JSONEncoder):
@@ -23,16 +32,25 @@ class DateTimeEncoder(json.JSONEncoder):
 
 
 def getLatestVersion():
-    version = requests.get(latestUrl)
+    version = requests.get(latestUrl, headers=headers)
     return int(version.text)
 
+
+releaseNotes = requests.get(releaseNotesUrl, headers=headers)
+if releaseNotes.status_code != 200:
+    print("Release notes returned error code "+str(releaseNotes.status_code))
+    exit(-1)
+
+productionTable = [(x[0] if len(x) > 0 else None) for x in releasePattern.findall(releaseNotes.text)]
+print("Production releases: "+str(len(productionTable)))
 
 def extractMetaData(zip_url, code):
     zipFile = remotezip.RemoteZip(zip_url)
     metadata = zipFile.read('ytl/.meta/manifest.json').decode('utf-8')
     creationDate = datetime(*zipFile.getinfo('ytl/.meta/manifest.json').date_time)
     metaJSON = json.loads(metadata)
-    return {"versionName": metaJSON['name'], "versionCode": code, "releaseDate": creationDate, "url": zip_url}
+    return {"versionName": metaJSON['name'], "versionCode": code, "releaseDate": creationDate, "url": zip_url,
+            "beta": (code > latestVersion or metaJSON["name"] not in productionTable)}
 
 
 latestVersion = getLatestVersion()
@@ -44,15 +62,17 @@ for i in range(knownOldestVersion, latestVersion + 5):
         meta = extractMetaData(url, i)
         if len(versions['koe']) < 1 or (
                 meta.get('versionName') != versions['koe'][len(versions['koe']) - 1].get('versionName') and meta.get(
-                'releaseDate') != versions['koe'][len(versions['koe']) - 1].get('releaseDate')):
+            'releaseDate') != versions['koe'][len(versions['koe']) - 1].get('releaseDate')):
             versions['koe'].append(meta)
-            print(str(meta['versionCode']) + ": " + meta['versionName'])
+            print(str(meta['versionCode']) + ": " + meta['versionName'] + (" (BETA)" if meta["beta"] else ""))
         try:
             url = "https://static.abitti.fi/etcher-usb/releases/" + str(i) + "/ktp-etcher.zip"
             meta = extractMetaData(url, i)
-            if len(versions['ktp']) < 1 or (meta.get('versionName') != versions['ktp'][len(versions['ktp'])-1].get('versionName') and meta.get('releaseDate') != versions['ktp'][len(versions['ktp'])-1].get('releaseDate')):
+            if len(versions['ktp']) < 1 or (meta.get('versionName') != versions['ktp'][len(versions['ktp']) - 1].get(
+                    'versionName') and meta.get('releaseDate') != versions['ktp'][len(versions['ktp']) - 1].get(
+                'releaseDate')):
                 versions['ktp'].append(meta)
-                print(str(meta['versionCode']) + ": " + meta['versionName'])
+                print(str(meta['versionCode']) + ": " + meta['versionName'] + (" (BETA)" if meta["beta"] else ""))
         except:
             print(str(i) + "_ktp: Failed!")
     except:
@@ -61,7 +81,7 @@ for i in range(knownOldestVersion, latestVersion + 5):
 jsonString = json.dumps(versions, cls=DateTimeEncoder)
 print(jsonString)
 
-print("Writing to "+filePath)
+print("Writing to " + filePath)
 jsonDump = open(filePath, "w")
 jsonDump.write(jsonString)
 jsonDump.close()
